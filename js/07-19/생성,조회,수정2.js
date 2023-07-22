671dfd3a23d8f16cfaf1b615766894b7959e4670
LKJ970524 <dlrbwjd7096@naver.com>
2023-07-22 22:55:54 +0900

    dom 추가학습

--- js/07-19/생성,조회,수정2.js
+++ js/07-19/생성,조회,수정2.js
@@ -16,7 +16,7 @@ console.log(JSON.parse(el.dataset.object));
 //--------------------------------------------------------------------------
 
 //* E.tagName
-//todo 요소의 태그 이름을 반환한다.
+//^ 요소의 태그 이름을 반환한다.
 /*
 const parentEl = document.querySelector('.parent')
 const childEl = document.querySelector('.child')
@@ -31,7 +31,7 @@ console.log(document.body.tagName);
 //--------------------------------------------------------------------------
 
 //* E.id
-//todo 요소의 `id` 속성 값을 얻거나 지정한다.
+//^ 요소의 `id` 속성 값을 얻거나 지정한다.
 /*
 const el = document.querySelector('.child')
 console.log(el.id);
@@ -43,7 +43,7 @@ console.log(el);
 //--------------------------------------------------------------------------
 
 //* E.className
-//todo 요소의 `class` 속성 값을 얻거나 지정한다.
+//^ 요소의 `class` 속성 값을 얻거나 지정한다.
 /*
 const el = document.querySelector('.child')
 console.log(el.className);
@@ -55,7 +55,7 @@ console.log(el);
 //--------------------------------------------------------------------------
 
 //* E.classList
-//todo 요소의 `class` 속성 값을 제어한다.
+//^ 요소의 `class` 속성 값을 제어한다.
 
 //! - `.add()` : 새로운 값을 추가
 //! - `.remove()` : 기존 값을 제거
@@ -78,30 +78,75 @@ el.addEventListener('click', ()=> {
 //--------------------------------------------------------------------------
 
 
-// E.style
-// 요소의 `style`속성(인라인 스타일)의 CSS 속성 값을 얻거나 지정한다.
-
+//* E.style
+//^ 요소의 `style`속성(인라인 스타일)의 CSS 속성 값을 얻거나 지정한다.
+//! 인라인 스타일방식은 css를 다룰 때 조심해야한다 (우선순위 점수가 1000점이기 때문에 id선택자(100점)나 class선택자(10점)로는 덮어쓸수가 없다)
+/*
 const el = document.querySelector('.child')
 
-// 개별지정!
+// 개별지정! //!남발하지말고 그냥css파일 하나 만들던 html에 style태그를 먹여라
 // el.style.width = '100px'
-// el.style.fontsize = '20px'
+// el.style.fontSize = '80px'
 // el.style.backgroundColor = 'green'
 // el.style.position = 'absolute'
 
-// 한번에 지정!
-// Object.assign(el.style, {
-//   width: '100px',
-//   fontsize: '20px',
-//   backgroundColor: 'green',
-//   position: 'absolute'
-// })
+// 한번에 지정! //! 정적 메소드를사용
+Object.assign(el.style, {
+  width: '100px',
+  fontSize: '20px',
+  backgroundColor: 'green',
+  position: 'absolute'
+})
+//~ 한두개의 css를 지정할대는 개별지정하는게 더 나을수는 있다 하지만 여러개의 css를 지정하게되면 한번에 지정하는 방식을 쓰는게 편하다
+
 
 console.log(el.style);
 console.log(el.style.width);
-console.log(el.style.fontsize);
+console.log(el.style.fontSize);
 console.log(el.style.backgroundColor);
 console.log(el.style.position);
-
+//! 지정된게없어서 전부 빈칸이다.
+*/
 //--------------------------------------------------------------------------
-//!16분 40초 듣기
+
+//* window.getComputedStyle()
+
+//^ 요소에 적용된 스타일 객체를 반환합니다.
+//~ js를 통한 style을 먹이는게 아닌 이미 적용된 css를 확인할수 있다.
+/*
+const el = document.querySelector('.child')
+const styles = window.getComputedStyle(el)
+
+console.log(styles.width);
+console.log(styles.fontSize);
+console.log(styles.backgroundColor);
+console.log(styles.position);
+console.log(styles); //? reset.css를 먹이면 나타나는게 어떻게 되는거냐 궁금하네;;;
+*/
+//--------------------------------------------------------------------------
+
+//* E.getAttribute() /E.setAttribute()
+
+//^ 요소에서 특정 속성 값을 얻거나 지정한다.
+//~ 속성이라는 개념은 html에서 말할때는 Attribute라는 단어를 사용하게된다.
+//~ css,js에서 속성이라는 개념은 property라는 단어를 사용하게된다.
+
+const el = document.querySelector('.child')
+
+el.setAttribute('title','Hello World!')
+console.log(el.getAttribute('title'));
+
+//---------------------------------------------------------------------------
+
+//* E.hasAttribute() /E.removeAttribute()
+
+//^ 요소에서 특정 속성을 확인하거나 제거한다.
+/*
+const el = document.querySelector('.child')
+
+console.log(el.hasAttribute('class'));
+
+el.removeAttribute('class')
+console.log(el.hasAttribute('class'));
+
+console.log(el);*/
